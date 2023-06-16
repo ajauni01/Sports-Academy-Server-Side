ae42e5b54f9ed7fd4c598a9e3bdc5c4849041235
AJHARUL ISLAM AUNIK <AJAUNI01@WSC.EDU>
2023-06-16 01:12:13 -0500

    instructors-api-added

--- index.js
+++ index.js
@@ -28,6 +28,50 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const registeredUsersCollection = client.db('powerPlaySports').collection('users');
+    const classesCollection = client.db('powerPlaySports').collection('allClasses');
+    const instructorCollection = client.db('powerPlaySports').collection('instructors');
+
+    // new user registration related apis
+    // store or insert user name and password in the database // save the user name, and email to the database ONLY when he doesn't already exist
+    app.post('/users', async (req, res) => {
+      console.log('registeredUsersCollection is getting hit')
+      const user = req.body;
+      const query = { email: user.email }
+      // check whether the user already exists
+      const existingUser = await registeredUsersCollection.findOne(query)
+      if (existingUser) {
+        return res.send({ message: 'user already exists' })
+      }
+      const result = await registeredUsersCollection.insertOne(user);
+      res.send(result);
+    })
+
+    // popular class related apis
+    app.get('/popularClasses', async (req, res) => {
+      const query = {}
+      // sort classes based on the number of students
+      const options = {
+        sort: { students: -1 }
+      }
+      const cursor = classesCollection.find(query, options)
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+    // popular instructor related apis
+    app.get('/instructors', async (req, res) => {
+      const query = {}
+      // sort instructors based on the number of students
+      const options = {
+        sort: { students: -1 }
+      }
+      const cursor = instructorCollection.find(query, options)
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
